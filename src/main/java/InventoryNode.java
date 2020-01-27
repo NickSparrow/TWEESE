3d3f1ca03d11059f2e7b170dfbd116a01b4deeab
NickSparrow <capnnicksparrow@gmail.com>
2020-01-27 12:12:35 +0100

    Ini

---
+++ src/main/java/InventoryNode.java
@@ -0,0 +1,12 @@
+package TWEditor;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+
+public class InventoryNode extends DefaultMutableTreeNode
+{
+  public InventoryNode(ItemTemplate itemTemplate)
+  {
+    super(itemTemplate);
+  }
+}
+
